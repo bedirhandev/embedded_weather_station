95e33976f053abc92580375b8562e6b09e16051d
Bedirhan <bedirhan_dincer@hotmail.com>
2020-10-18 22:46:49 +0200

    Initial commit to develop branch. New updates on task communication, neater and tighter code, introducing a new file called .ldr. Future improvements will be done on file .ldr.

---
+++ main/ldr.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "esp_system.h"
+
+void ldr_task(void *pvParameters);
--- main/main.c
+++ main/main.c
@@ -1,20 +1,27 @@
 #include "sender.h"
 #include "si7021.h"
+#include "ldr.h"
 
 #include "nvs_flash.h"
+#include "esp_log.h"
+
+static xQueueHandle queue;
 
 void app_main(void)
 {
-	esp_err_t ret = nvs_flash_init();
-	if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		ret = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(ret);
+	queue = xQueueCreate(2, sizeof(char*));
+
+//	esp_err_t ret = nvs_flash_init();
+//	if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+//		ESP_ERROR_CHECK(nvs_flash_erase());
+//		ret = nvs_flash_init();
+//	}
+//	ESP_ERROR_CHECK(ret);
 	
 	initialise_wifi();
 
-	//start i2c task
-	xTaskCreate(i2c_task, "i2c_task", 8096, NULL, tskIDLE_PRIORITY + 2, NULL);
-	xTaskCreate(http_post_task, "http_post_task", 8096, NULL, tskIDLE_PRIORITY + 1, NULL);
+	// in total around 9KB space occupied
+	xTaskCreate(ldr_task, "ldr_task", 1024 * 1, (void*) queue, tskIDLE_PRIORITY + 2, NULL);
+	xTaskCreate(i2c_task, "i2c_task", 1024 * 2, (void*) queue, tskIDLE_PRIORITY + 2, NULL);
+	xTaskCreate(http_post_task, "http_post_task", 1024 * 3, (void*) queue, tskIDLE_PRIORITY + 1, NULL);
 }
